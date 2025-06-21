0b27192071078f52a85025467e6de52d64a2e9fa
agiabhishek <abhishekxxxybl@gmail.com>
2025-06-21 15:58:18 +0530

    first commit

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,7 @@
+import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
+import "dotenv/config";
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
@@ -59,7 +61,7 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
+  const port = 5002;
   server.listen({
     port,
     host: "0.0.0.0",
